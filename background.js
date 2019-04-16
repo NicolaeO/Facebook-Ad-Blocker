40be5b6aa3de695ed83d4dd4566ed394211694da
nicu104@gamil.com <nicu104@gamil.com>
2019-04-16 15:12:11 -0500

    first commit

---
+++ background.js
@@ -0,0 +1,38 @@
+
+// for the first time call
+cleanUp();
+
+// Clean up every time user scrolls
+let last_known_scroll_position = 0;
+let ticking = false;
+
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      cleanUp();
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
+
+
+function cleanUp(){
+    console.log("scrolled");
+    var elements = document.querySelectorAll('[data-testid="fbfeed_story"]');
+    for(let element of elements){
+        var sponsoredClass = element.getElementsByClassName("z_1p4ow-s_n_");
+        var sponsoredText = "";
+        for(let spEl of sponsoredClass){
+            sponsoredText += spEl.textContent;
+        }
+        if(sponsoredText === "Sponsored"){
+            element.remove();
+            console.log("Removed!");
+        }
+    }
+}
