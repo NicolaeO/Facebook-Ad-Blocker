f9aafd36e4af893a272a04e38c0e9e5ac7088843
nicu104@gamil.com <nicu104@gamil.com>
2019-04-17 09:42:57 -0500

    changed listener to look for DOM changes instead of scrolling

--- background.js
+++ background.js
@@ -11,22 +11,7 @@ const sponsoredElement = [
 
 // for the first time call
 console.log("Started cleaning up AD mess from facebook!!");
-cleanUp();
-
-// Clean up every time user scrolls
-let last_known_scroll_position = 0;
-let ticking = false;
-window.addEventListener('scroll', function(e) {
-  last_known_scroll_position = window.scrollY;
-  if (!ticking) {
-    window.requestAnimationFrame(function() {
-      cleanUp();
-      ticking = false;
-    });
-    ticking = true;
-  }
-});
-
+document.addEventListener('DOMNodeInserted', cleanUp);
 
 function cleanUp(){
     var elements = document.querySelectorAll(parentElement);
