ebbec1dff7046784916f0cba352690d095fd6b72
nicu104@gamil.com <nicu104@gamil.com>
2019-04-17 01:11:39 -0500

    nothing modified

--- background.js
+++ background.js
@@ -1,10 +1,18 @@
 
+
 //these two variables will be soon changed ...
-const parentElement = '[data-testid="fbfeed_story"]';
-const sponsoredElement = "z_1p4ow-s_n_";
+const parentElement = [
+    '[data-testid="fbfeed_story"]',
+    "fbfeed_story"
+];
+const sponsoredElement = [
+    "z_1p4ow-s_n_"
+];
 
 // for the first time call
+console.log("Started cleaning up AD mess from facebook!!");
 cleanUp();
+
 // Clean up every time user scrolls
 let last_known_scroll_position = 0;
 let ticking = false;
