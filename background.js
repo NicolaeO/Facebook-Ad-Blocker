b8ab2d1889d5765bb8f24a81fbe010404ee62fc9
nicu104@gamil.com <nicu104@gamil.com>
2019-04-22 10:56:39 -0500

    modified class finding method

--- background.js
+++ background.js
@@ -6,8 +6,20 @@ const parentElement = [
     "fbfeed_story"
 ];
 const sponsoredElement = [
-    "z_1p4ow-s_n_"
+    // "z_1p4ow-s_n_"
+    "b"
 ];
+const keyWordsToLookFor = [
+    "Sponsored",
+    "Sponsorizat",
+    "9 hrs",
+    "Sponsored In Other Languages"
+]
+var counter = 0;
+
+//this code is for geting data file from containig folder
+// var str = chrome.runtime.getURL("./data.json");
+// console.log(str);
 
 // for the first time call
 console.log("Started cleaning up AD mess from facebook!!");
@@ -16,13 +28,14 @@ document.addEventListener('DOMNodeInserted', cleanUp);
 function cleanUp(){
     var elements = document.querySelectorAll(parentElement);
     for(let element of elements){
-        var sponsoredClass = element.getElementsByClassName(sponsoredElement);
-        var sponsoredText = "";
+        var sponsoredClass = element.querySelectorAll(sponsoredElement);
         for(let spEl of sponsoredClass){
-            sponsoredText += spEl.textContent;
-        }
-        if(sponsoredText === "Sponsored"){
-            element.remove();
+            let text = spEl.textContent.split("").filter(_=> _!== "-").join("");
+            if(keyWordsToLookFor.includes(text)){
+                console.log(element);
+                element.remove();
+                counter++;
+            }
         }
     }
 }
